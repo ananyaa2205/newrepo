7528d1cd7463b7f00a0fd8e0c70c69dbe2e35c30
ananyaa2205 <saratheananya22@gmail.com>
2023-03-09 13:12:35 +0530

    file renewed

--- ComplexCalculator.java
+++ ComplexCalculator.java
@@ -4,12 +4,13 @@ import java.util.*;
 public class ComplexCalculator {
 
 	public static void main(String[] argss) {
-		System.out.println("Hello"+" enter ");
-//		 String input ="1 fish 2 fish 3 fish 4";
-		 Scanner sc = new Scanner(System.in).useDelimiter("\\sfish\\s ");
-		 System.out.println(sc.nextInt()+" "+sc.nextInt()+" "+sc.nextInt()+" "+sc.nextInt());
-		 sc.close();
-//		 System.out.println(sc.next());
+		System.out.println("enter a complex number");
+		System.out.println("enter x of complex number");
+        Scanner sc = new Scanner(System.in);
+		int x= sc.nextInt();
+		System.out.println("enter y of complex number");
+		int y= sc.nextInt();
+		 System.out.println(x+"+"+y+"i");
 //		 System.out.println(sc.next());
 //		 System.out.println(sc.next());
 
